a5788c455855d770e898e6efe03577bd0d16abeb
Soulter <905617992@qq.com>
2025-07-25 15:05:19 +0800

    fix: ensure environment variables are overridden when loading

--- backend/main.py
+++ backend/main.py
@@ -8,10 +8,10 @@ PROJ_DIR = os.path.dirname(os.path.dirname(__file__))
 ENV = os.getenv("ENV", "dev")
 if ENV == "dev":
     logger.info("Loading DEV environment variables")
-    load_dotenv(os.path.join(PROJ_DIR, ".env.dev"))
+    load_dotenv(os.path.join(PROJ_DIR, ".env.dev"), override=True)
 elif ENV == "prod":
     logger.info("Loading PROD environment variables")
-    load_dotenv(os.path.join(PROJ_DIR, ".env.prod"))
+    load_dotenv(os.path.join(PROJ_DIR, ".env.prod"), override=True)
 
 from router import app # noqa: E402
 
