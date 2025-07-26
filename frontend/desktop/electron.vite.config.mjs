c3b4ccbdf84e6029a0dd45980415a9e4cf982cd4
Fridemn <702625325@qq.com>
2025-07-26 20:26:58 +0800

    update

--- frontend/desktop/electron.vite.config.mjs
+++ frontend/desktop/electron.vite.config.mjs
@@ -19,7 +19,7 @@ export default defineConfig({
     server: {
       proxy: {
         '/api': {
-          target: 'http://localhost:8000',
+          target: 'https://memora.soulter.top',
           changeOrigin: true,
           secure: false
         },
